1f88840cb904d2fcf9d934d1fff70ca9fdf758cf
F4D4 <verdievf@inbox.ru>
2024-06-02 14:40:33 +0300

    update pom.xml

--- server/src/main/java/com/f4d4/server/rulers/DatabaseRuler.java
+++ server/src/main/java/com/f4d4/server/rulers/DatabaseRuler.java
@@ -29,7 +29,7 @@ public class DatabaseRuler {
 
     private final String CHECK_IF_USERID_IS_CORRECT = "select user_id from ticket where id=?;";
 
-    private final String GET_USER_ID="select id from users where login = ?;";
+    private final String GET_USER_ID = "select id from users where login = ?;";
     private final String INSERT_USER = "insert into users(login , password_hash , salt) values(?,?,?);";
 
     private final String LOAD_DB = " select * from ticket;";
@@ -41,8 +41,8 @@ public class DatabaseRuler {
     private final String REMOVE_BY_ID = "delete from ticket where id = ?;";
     private final String CLEAR = "delete from ticket where user_id = ?;";
 
-    public void connect(){
-        try{
+    public void connect() {
+        try {
             connection = DriverManager.getConnection(URL);
             log.info("Подключение к базе данных успешно");
         } catch (SQLException e) {
@@ -54,10 +54,10 @@ public class DatabaseRuler {
 
     public PriorityQueue<Ticket> loadCollection() {
         PriorityQueue<Ticket> result = new PriorityQueue<>();
-        try{
+        try {
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(LOAD_DB);
-            while(resultSet.next()){
+            while (resultSet.next()) {
                 long id = resultSet.getLong("id");
                 String name = resultSet.getString("name");
                 int x = resultSet.getInt("x_coordinate");
@@ -72,19 +72,19 @@ public class DatabaseRuler {
                 EventType eventType = EventType.valueOf(resultSet.getString("event_type"));
                 int userId = resultSet.getInt("user_id");
                 String login = resultSet.getString("login");
-                result.add(new Ticket(id,name,new Coordinates(x,y),price,discount,type,creationDate,
-                        new Event(eventID,eventName,minAge,eventType) , userId, login));
+                result.add(new Ticket(id, name, new Coordinates(x, y), price, discount, type, creationDate,
+                        new Event(eventID, eventName, minAge, eventType), userId, login));
             }
-        }catch (SQLException e){
+        } catch (SQLException e) {
             log.warn("Не удалось полностью загрузить коллекцию");
         }
         return result;
     }
 
-    public Ticket getLastTicket() throws SQLException{
+    public Ticket getLastTicket() throws SQLException {
         Statement statement = connection.createStatement();
         ResultSet resultSet = statement.executeQuery(LAST_TICKET);
-        if(resultSet.next()){
+        if (resultSet.next()) {
             long id = resultSet.getLong("id");
             String name = resultSet.getString("name");
             int x = resultSet.getInt("x_coordinate");
@@ -99,49 +99,49 @@ public class DatabaseRuler {
             EventType eventType = EventType.valueOf(resultSet.getString("event_type"));
             int userId = resultSet.getInt("user_id");
             String login = resultSet.getString("login");
-            return new Ticket(id,name,new Coordinates(x,y),price,discount,type,creationDate,
-                    new Event(eventID,eventName,minAge,eventType) , userId , login);
+            return new Ticket(id, name, new Coordinates(x, y), price, discount, type, creationDate,
+                    new Event(eventID, eventName, minAge, eventType), userId, login);
         }
         return null;
     }
 
-    public void insertUser(String login , String password , String salt) throws SQLException{
+    public void insertUser(String login, String password, String salt) throws SQLException {
         PreparedStatement addUser = connection.prepareStatement(INSERT_USER);
-        addUser.setString(1,login);
-        addUser.setString(2,password);
-        addUser.setString(3,salt);
+        addUser.setString(1, login);
+        addUser.setString(2, password);
+        addUser.setString(3, salt);
         addUser.executeUpdate();
     }
 
-    public boolean checkUser(String login , String password) throws SQLException{
+    public boolean checkUser(String login, String password) throws SQLException {
         PreparedStatement checkStatement = connection.prepareStatement(CHECK_USER);
-        checkStatement.setString(1,login);
-        ResultSet resultSetUser =  checkStatement.executeQuery();
-        if(resultSetUser.next()){
+        checkStatement.setString(1, login);
+        ResultSet resultSetUser = checkStatement.executeQuery();
+        if (resultSetUser.next()) {
             var passwordHash = resultSetUser.getString("password_hash");
             var salt = resultSetUser.getString("salt");
-            var hashPasswordWithSalt = PasswordHashing.hashPasswordWithSalt(password,salt);
+            var hashPasswordWithSalt = PasswordHashing.hashPasswordWithSalt(password, salt);
             var result = passwordHash.equals(hashPasswordWithSalt);
             return result;
-        }else {
+        } else {
             return false;
         }
 
     }
 
-    public int getUserID(String login) throws SQLException{
+    public int getUserID(String login) throws SQLException {
         PreparedStatement checkStatement = connection.prepareStatement(GET_USER_ID);
-        checkStatement.setString(1,login);
+        checkStatement.setString(1, login);
         ResultSet resultSet = checkStatement.executeQuery();
-        if(resultSet.next()){
+        if (resultSet.next()) {
             var result = resultSet.getInt("id");
             return result;
-        }else{
+        } else {
             return -1;
         }
     }
 
-    public void insertTicket(Ticket ticket , String login)throws SQLException{
+    public void insertTicket(Ticket ticket, String login) throws SQLException {
         PreparedStatement preparedStatement = connection.prepareStatement(ADD_TICKET);
         String name = ticket.getName();
         int x = ticket.getXcoordinate();
@@ -154,22 +154,22 @@ public class DatabaseRuler {
         long minAge = ticket.getEventMinage();
         String eventType = String.valueOf(ticket.getEventType());
         int userId = ticket.getUser_id();
-        preparedStatement.setString(1,name);
-        preparedStatement.setInt(2,x);
-        preparedStatement.setDouble(3,y);
-        preparedStatement.setLong(4,price);
-        preparedStatement.setInt(5,discount);
-        preparedStatement.setString(6,ticketType);
-        preparedStatement.setString(7,crationDate);
-        preparedStatement.setString(8,eventName);
-        preparedStatement.setLong(9,minAge);
-        preparedStatement.setString(10,eventType);
-        preparedStatement.setInt(11,userId);
-        preparedStatement.setString(12,login);
+        preparedStatement.setString(1, name);
+        preparedStatement.setInt(2, x);
+        preparedStatement.setDouble(3, y);
+        preparedStatement.setLong(4, price);
+        preparedStatement.setInt(5, discount);
+        preparedStatement.setString(6, ticketType);
+        preparedStatement.setString(7, crationDate);
+        preparedStatement.setString(8, eventName);
+        preparedStatement.setLong(9, minAge);
+        preparedStatement.setString(10, eventType);
+        preparedStatement.setInt(11, userId);
+        preparedStatement.setString(12, login);
         preparedStatement.executeUpdate();
     }
 
-    public void updateTicket(Ticket ticket,long id) throws SQLException{
+    public void updateTicket(Ticket ticket, long id) throws SQLException {
         PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_TICKET);
         String name = ticket.getName();
         int x = ticket.getXcoordinate();
@@ -180,44 +180,44 @@ public class DatabaseRuler {
         String eventName = ticket.getEventName();
         long minAge = ticket.getEventMinage();
         String eventType = String.valueOf(ticket.getEventType());
-        preparedStatement.setString(1,name);
-        preparedStatement.setInt(2,x);
-        preparedStatement.setDouble(3,y);
-        preparedStatement.setLong(4,price);
-        preparedStatement.setInt(5,discount);
-        preparedStatement.setString(6,ticketType);
-        preparedStatement.setString(7,eventName);
-        preparedStatement.setLong(8,minAge);
-        preparedStatement.setString(9,eventType);
-        preparedStatement.setLong(10,id);
+        preparedStatement.setString(1, name);
+        preparedStatement.setInt(2, x);
+        preparedStatement.setDouble(3, y);
+        preparedStatement.setLong(4, price);
+        preparedStatement.setInt(5, discount);
+        preparedStatement.setString(6, ticketType);
+        preparedStatement.setString(7, eventName);
+        preparedStatement.setLong(8, minAge);
+        preparedStatement.setString(9, eventType);
+        preparedStatement.setLong(10, id);
         preparedStatement.executeUpdate();
     }
 
-    public void removeTicketById(long id) throws SQLException{
+    public void removeTicketById(long id) throws SQLException {
         PreparedStatement preparedStatement = connection.prepareStatement(REMOVE_BY_ID);
-        preparedStatement.setLong(1,id);
+        preparedStatement.setLong(1, id);
         preparedStatement.executeUpdate();
     }
 
-    public int isCorrectID(long id) throws SQLException{
+    public int isCorrectID(long id) throws SQLException {
         PreparedStatement preparedStatement = connection.prepareStatement(CHECK_IF_USERID_IS_CORRECT);
-        preparedStatement.setLong(1,id);
+        preparedStatement.setLong(1, id);
         ResultSet resultSet = preparedStatement.executeQuery();
-        if(resultSet.next()){
+        if (resultSet.next()) {
             return resultSet.getInt("user_id");
-        }else{
+        } else {
             return -1;
         }
     }
 
-    public void removeFirstTicket() throws SQLException{
+    public void removeFirstTicket() throws SQLException {
         Statement statement = connection.createStatement();
         statement.executeUpdate(REMOVE_FIRST_TICKET);
     }
 
-    public void clear(int id) throws SQLException{
+    public void clear(int id) throws SQLException {
         PreparedStatement preparedStatement = connection.prepareStatement(CLEAR);
-        preparedStatement.setInt(1,id);
+        preparedStatement.setInt(1, id);
         preparedStatement.executeUpdate();
     }
 
